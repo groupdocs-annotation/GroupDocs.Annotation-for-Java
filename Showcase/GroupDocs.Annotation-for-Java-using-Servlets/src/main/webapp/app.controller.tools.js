51ca116478f8bbb080429f5b1a51a9087a562080
Ali Ahmed Sahi <ali.ahmed@groupdocs.com>
2017-10-03 09:30:36 +0500

    Distance, Underline and Strikeout Annotation Added

--- Showcase/GroupDocs.Annotation-for-Java-using-Servlets/src/main/webapp/app.controller.tools.js
+++ Showcase/GroupDocs.Annotation-for-Java-using-Servlets/src/main/webapp/app.controller.tools.js
@@ -6,6 +6,9 @@
         $scope.selectRectangleTool = function ($event) {
             $rootScope.selectedDrawingTool = 'rectangle';
         };
+        $scope.selectDistanceTool = function ($event) {
+            $rootScope.selectedDrawingTool = 'distance';
+        };
         $scope.selectArrowTool = function ($event) {
             $rootScope.selectedDrawingTool = 'arrow';
         };
@@ -17,6 +20,12 @@
         };
         $scope.selectPointTool = function ($event) {
             $rootScope.selectedDrawingTool = 'point';
+        };
+        $scope.selectUnderlineTool = function ($event) {
+            $rootScope.selectedDrawingTool = 'underline';
+        };
+        $scope.selectStrikeoutTool = function ($event) {
+            $rootScope.selectedDrawingTool = 'strikeout';
         };
         
         $scope.selectSelectTool();
